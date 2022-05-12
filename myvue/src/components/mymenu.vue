32bb0c1f397e5d1bf3eb802b37962e79a2547fd1
Caroline-gutianle <2311601671@qq.com>
2022-05-12 10:40:45 +0800

    vue

---
+++ myvue/src/components/mymenu.vue
@@ -0,0 +1,95 @@
+<template>
+  <div>
+    <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+      <el-radio-button :label="false">展开</el-radio-button>
+      <el-radio-button :label="true">收起</el-radio-button>
+    </el-radio-group>
+    <el-menu   router default-active="path" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+
+      <el-submenu index="1">
+
+        <template slot="title">
+          <i class="el-icon-edit"></i>
+          <span slot="title">管理</span>
+        </template>
+
+        <el-menu-item-group>
+          <el-menu-item index="add_deletelike_person">person数据管理(之后删</el-menu-item>
+        </el-menu-item-group>
+
+        <el-menu-item-group>
+          <el-menu-item index="student_denglu">学生登录</el-menu-item>
+        </el-menu-item-group>
+
+        <el-menu-item-group>
+          <el-menu-item index="teacher_denglu">老师登录</el-menu-item>
+        </el-menu-item-group>
+
+        <el-menu-item-group>
+          <el-menu-item index="tourist_denglu">游客访问</el-menu-item>
+        </el-menu-item-group>
+
+        <el-menu-item-group>
+          <el-menu-item index="student_caozuo">学生操作(之后改</el-menu-item>
+        </el-menu-item-group>
+
+        <el-menu-item-group>
+          <el-menu-item index="teacher_caozuo">老师操作(之后改</el-menu-item>
+        </el-menu-item-group>
+
+        <el-menu-item-group>
+          <el-menu-item index="teacher_caozuo2">老师进阶操作(之后改</el-menu-item>
+        </el-menu-item-group>
+
+        <el-menu-item-group>
+          <el-menu-item index="tiku">题库内容(之后改</el-menu-item>
+        </el-menu-item-group>
+
+          <el-menu-item index="add_deletelike_user" >user数据管理(之后删</el-menu-item>
+
+
+      </el-submenu>
+
+      <el-menu-item index="home">
+        <i class="el-icon-location"></i>
+        <span slot="title">person数据库</span>
+      </el-menu-item>
+      <el-menu-item index="userhome">
+        <i class="el-icon-location"></i>
+        <span slot="title">users数据库</span>
+      </el-menu-item>
+
+    </el-menu>
+  </div>
+</template>
+
+<style>
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 100vh;
+}
+</style>
+
+<script>
+export default {
+  name: "mymenu",
+  data() {
+    return {
+      isCollapse: true,
+      path:this.$route.path
+    };
+  },
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath);
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ myvue/src/components/teacher_caozuo.vue
@@ -0,0 +1,133 @@
+<template>
+  <div >
+    <div style="height: 40px">
+    </div>
+    <div style="text-align: center;width: 100%; padding-left: 400px;margin: auto">
+
+      <el-card style="width: 40%; margin: 10px">
+        <i class="el-icon-tickets"></i>
+        <p>查看学生选题情况</p>
+
+        <div style="text-align: center">
+          <el-button type="primary" icon="el-icon-edit" @click="save('form')">查看</el-button>
+        </div>
+
+        <p>！：蹦到进阶操作</p>
+      </el-card>
+
+      <el-card style="width: 40%; margin: 10px">
+
+        <i class="el-icon-link"></i>
+        <p>资料</p>
+
+        <el-upload
+          class="upload-demo"
+          drag
+          action="https://jsonplaceholder.typicode.com/posts/"
+          multiple>
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+        </el-upload>
+      </el-card>
+
+      <el-card style="width: 40%; margin: 10px">
+
+        <i class="el-icon-lock"></i>
+        <p>修改密码</p>
+
+        <el-form :model="form"  class="demo-ruleForm" label-position="left" label-width="80px"  status-icon :rules="rules" ref="form">
+
+          <el-form-item label="tpass" :label-width="formLabelWidth" prop="tpass">
+            <el-input style="width: 200px"
+                      placeholder="请输入新密码"
+                      v-model="form.tpass"
+                      clearable>
+            </el-input>
+          </el-form-item>
+
+          <div style="margin: 50px;"></div>
+        </el-form>
+
+        <div style="text-align: center">
+          <el-button type="primary" icon="el-icon-edit" @click="save('form')">确认</el-button>
+        </div>
+
+      </el-card>
+
+
+    </div>
+  </div>
+</template>
+
+<script>
+import request from "../util/request";
+
+export default {
+  name: "teacher_caozuo",
+  data() {
+    return {
+      form:{
+        kno:'',
+        tpass:'',
+      },
+      search: '',
+    };
+  },
+
+  methods: {
+    save(form) {
+      this.$refs[form].validate((valid) => {
+        if (valid) {
+
+          request.post("/api/person/addPerson", this.form).then(res => {
+
+            console.log(res.data); //打印出来
+            if ((res.data !== 0)) {
+              if ((res.data === 1)){
+                this.$message({
+                  type: "success",
+                  message: "成功添加"+this.form.name
+                })
+              }else{
+                this.$confirm("用户名"+this.form.username+"已存在，需要修改信息吗？", '提示', {
+                  confirmButtonText: '确定',
+                  cancelButtonText: '取消',
+                  type: 'warning'
+                }).then(() => {
+                  request.put("/api/person/updatePerson",this.form).then(res=> {
+                    console.log(res) //打印出来
+                    if (res.code !== '0') {
+                      this.$message({
+                        type: "success",
+                        message: "修改成功"
+                      })
+
+                    }
+                  });
+
+                }).catch(() => {
+                  this.$message({
+                    type: 'info',
+                    message: '已取消修改'
+                  });
+                });
+              }
+
+            } else {
+              this.$message({
+                type: "error",
+                message: "名称"+this.form.name+"重复,请重新填写"
+              })
+            }
+          })
+        }
+      });
+    },
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
